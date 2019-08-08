9093bbf0d54b09ba4a5b6b9bd3e3d2aca02fb643
ing <inyoman_gurnitha@yahoo.com>
2019-08-08 12:50:25 +0700

    4. Creating Our Products App Model Part 1

---
+++ README.md
@@ -0,0 +1,11 @@
+## 3. Creating Our Django Project
+
+django-admin startproject src .
+
+## 4. Creating Our Products App Model Part 1
+
+python manage.py startapp app_product
+
+register the app to the project
+
+create Product model
--- app_product/models.py
+++ app_product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+	## Each product has its own condition (new and used)
+	CONDITION_TYPE = (
+		("New", "New"),
+		("Used", "Used")
+	)
+
+	## It contains all the products information
+	name 				= models.CharField(max_length=100)
+	description = models.TextField(max_length=500)
+	condition 	= models.CharField(max_length=100, choices=CONDITION_TYPE)
+	price 			= models.DecimalField(max_digits=10, decimal_places=5)
+	created 		= models.DateTimeField()
+
+	# To change the display from "Product object(1)" to "name"
+	def __str__(self):
+		return self.name
