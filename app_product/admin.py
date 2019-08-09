e423b454642e9f26c1a87a6089faece1eba6921b
ing <inyoman_gurnitha@yahoo.com>
2019-08-09 10:46:48 +0700

    7.+( Display ProductImages) Adding Brand & Product Images Models

--- app_product/admin.py
+++ app_product/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product, Category, Brand
+from .models import Product, Category, Brand, ProductImages
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(ProductImages)
